904a421fb04718a46b8738fe429bf4d024a9b45a
Georgiy Nedielko <georgiynge2025@gmail.com>
2025-11-21 09:18:54 +0100

    Fix DZ4_1: remove sleep, add WebDriverWait and update screenshot

--- DZ4/DZ4_1.py
+++ DZ4/DZ4_1.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 # Определяем путь текущей папки
@@ -15,7 +16,11 @@ try:
 
     driver.find_element(By.ID, "newButtonName").send_keys("ITCH")
     driver.find_element(By.ID, "updatingButton").click()
-    sleep(1)
+
+    # Ждём, пока текст кнопки изменится на "ITCH"
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element((By.ID, "updatingButton"), "ITCH")
+    )
 
     # Сохраняем скриншот в ту же папку
     driver.save_screenshot(screenshot_path)
